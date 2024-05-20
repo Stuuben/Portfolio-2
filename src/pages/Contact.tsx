d943019bcdd759c162c6b0861d5ef001079f0709
Adam Stuborn <a.stuborn@gmail.com>
2024-05-21 00:50:37 +0200

    add styling

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -7,6 +7,12 @@ export const Contact = () => {
   return (
     <div className="contact-wrapper">
       <h3>Contact</h3>
+      <p className="contact-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis non
+        quis reiciendis quibusdam id quia omnis provident molestias saepe! Quos
+        provident earum, nostrum alias quo omnis! Officiis asperiores dolor
+        accusamus!
+      </p>
       <Form></Form>
     </div>
   );
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,30 +1,55 @@
 export const Projects = () => {
   return (
     <div className="projects-wrapper">
-      {/* <div className="projects-inner"> */}
       <h3 style={{ textAlign: "center", paddingBottom: "1rem" }}>Projects</h3>
-      <div>
-        <div className="center">
-          <a
-            className="link"
-            rel="stylesheet"
-            href="https://josefinstuborn.netlify.app/"
-          >
-            Josefins hemsida
-          </a>
-          <img src="/jstuub.png" alt="Josefins hemsida" width={340} />
+      <div className="projects-inner">
+        {" "}
+        <div className="project-item">
+          <div className="center">
+            <h3>Examensarbete</h3>
+            <img src="/jstuub.png" alt="Josefins hemsida" width={340} />
 
-          <p className="description">
-            Som en del av mitt examensarbete har jag utvecklat en webbaserad
-            konstnärlig portfolio för min syster. Syftet med detta projekt var
-            att skapa en dynamisk och estetiskt tilltalande plattform där hon
-            kan visa upp och dela med sig av sin konst. Webbplatsen är utformad
-            för att ge besökarna en djupare förståelse för hennes konstnärliga
-            uttryck och processer.
-          </p>
+            <p className="description">
+              Som en del av mitt examensarbete har jag utvecklat en webbaserad
+              konstnärlig portfolio för min syster. Syftet med detta projekt var
+              att skapa en dynamisk och estetiskt tilltalande plattform där hon
+              kan visa upp och dela med sig av sin konst. Webbplatsen är
+              utformad för att ge besökarna en djupare förståelse för hennes
+              konstnärliga uttryck och processer.
+            </p>
+            <button className="button-project">
+              <a
+                className="link"
+                href="https://josefinstuborn.netlify.app/"
+                target="_blank"
+              >
+                Gå till hemsida
+              </a>
+            </button>
+          </div>
+        </div>
+        <div className="project-item">
+          <div className="center">
+            <a
+              className="link"
+              rel="stylesheet"
+              href="https://josefinstuborn.netlify.app/"
+            >
+              Examensarbete
+            </a>
+            <img src="/jstuub.png" alt="Josefins hemsida" width={340} />
+
+            <p className="description">
+              Som en del av mitt examensarbete har jag utvecklat en webbaserad
+              konstnärlig portfolio för min syster. Syftet med detta projekt var
+              att skapa en dynamisk och estetiskt tilltalande plattform där hon
+              kan visa upp och dela med sig av sin konst. Webbplatsen är
+              utformad för att ge besökarna en djupare förståelse för hennes
+              konstnärliga uttryck och processer.
+            </p>
+          </div>
         </div>
       </div>
     </div>
-    // </div>
   );
 };
