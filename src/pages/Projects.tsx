e231da3b1488c7fac2a55e848a98200cd10fdf0e
Adam Stuborn <a.stuborn@gmail.com>
2024-07-10 22:05:05 +0200

    refactor project component

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,289 +1,112 @@
 import { useState } from "react";
 import "../scss/Projects.scss";
 
+interface ProjectItemProps {
+  title: string;
+  description: string;
+  image: string;
+  link: string;
+  isVisible: boolean;
+  buttonText: string;
+}
+
 export const Projects = () => {
-  const [showBacksideZoo, setShowBacksideZoo] = useState(true);
-  const [showBacksideExamen, setShowBacksideExamen] = useState(true);
-  const [showBacksideGodis, setShowBacksideGodis] = useState(true);
-  const [showBacksideTicTacToe, setShowBacksideTicTacToe] = useState(true);
-  const [showBacksideResturant, setShowBacksideResturant] = useState(true);
-  const [showBacksideGenerator, setShowBacksideGenerator] = useState(true);
-  const handleMouseOverZoo = () => {
-    console.log("over zoo");
-
-    setShowBacksideZoo(false);
-  };
-  const handleMouseLeaveZoo = () => {
-    setShowBacksideZoo(true);
-  };
-
-  const handleMouseOverExamen = () => {
-    setShowBacksideExamen(false);
-  };
-  const handleMouseLeaveExamen = () => {
-    setShowBacksideExamen(true);
-  };
-  const handleMouseOverGodis = () => {
-    setShowBacksideGodis(false);
-  };
-  const handleMouseLeaveGodis = () => {
-    setShowBacksideGodis(true);
-  };
-
-  const handleMouseOverTicTacToe = () => {
-    setShowBacksideTicTacToe(false);
-  };
-  const handleMouseLeaveTicTacToe = () => {
-    setShowBacksideTicTacToe(true);
-  };
-  const handleMouseOverResturant = () => {
-    setShowBacksideResturant(false);
-  };
-  const handleMouseLeaveResturant = () => {
-    setShowBacksideResturant(true);
-  };
-
-  const handleMouseOverGenerator = () => {
-    setShowBacksideGenerator(false);
-  };
-  const handleMouseLeaveGenerator = () => {
-    setShowBacksideGenerator(true);
+  const ProjectItem = ({
+    title,
+    description,
+    image,
+    link,
+    buttonText,
+  }: ProjectItemProps) => {
+    const [showBackside, setShowBackside] = useState(true);
+    const handleMouseOver = () => {
+      setShowBackside(false);
+    };
+    const handleMouseLeave = () => {
+      setShowBackside(true);
+    };
+    return (
+      <div
+        className="project-item"
+        onMouseOver={handleMouseOver}
+        onMouseLeave={handleMouseLeave}
+      >
+        {showBackside ? (
+          <div className="frontside">
+            <img src={image} alt={title} width={340} height={240} />
+          </div>
+        ) : (
+          <div className={`backside ${showBackside ? "visible" : ""}`}>
+            <div className="description-wrapper">
+              <h4 className="project-title">{title}</h4>
+              <p className="description">{description}</p>
+            </div>
+            <div className="button-wrapper">
+              <button className="button-project">
+                <a className="link" href={link} target="_blank">
+                  {buttonText}
+                </a>
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    );
   };
 
   return (
     <div className="projects-wrapper">
       <h3 style={{ textAlign: "center", paddingBottom: "1rem" }}>Projects</h3>
       <div className="projects-inner">
-        <div
-          className="project-item"
-          onMouseOver={() => handleMouseOverExamen()}
-          onMouseLeave={() => handleMouseLeaveExamen()}
-        >
-          {showBacksideExamen ? (
-            <div className="frontside">
-              <img
-                src="/joes.png"
-                alt="Josefins hemsida"
-                width={340}
-                height={240}
-              />
-            </div>
-          ) : (
-            <div className={`backside ${showBacksideExamen ? "visible" : ""}`}>
-              <div className="description-wrapper">
-                <h4 className="project-title">Examensarbete</h4>
-                <p className="description-examen">
-                  Som en del av mitt examensarbete har jag utvecklat en
-                  webbaserad konstnärlig portfolio för min syster. Syftet med
-                  detta projekt var att skapa en dynamisk och estetiskt
-                  tilltalande plattform där hon kan visa upp och dela med sig av
-                  sin konst. Webbplatsen är utformad för att ge besökarna en
-                  djupare förståelse för hennes konstnärliga uttryck och
-                  processer.
-                </p>
-              </div>
-              <div className="button-wrapper">
-                <button className="button-project">
-                  <a
-                    className="link"
-                    href="https://josefinstuborn.netlify.app/"
-                    target="_blank"
-                  >
-                    Se hemsida
-                  </a>
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
-        <div
-          className="project-item"
-          onMouseOver={() => handleMouseOverGenerator()}
-          onMouseLeave={() => handleMouseLeaveGenerator()}
-        >
-          {showBacksideGenerator ? (
-            <div className="frontside">
-              <img
-                src="/generator.png"
-                alt="homepage"
-                width={340}
-                height={240}
-              />
-            </div>
-          ) : (
-            <div
-              className={`backside ${showBacksideGenerator ? "visible" : ""}`}
-            >
-              <div className="description-wrapper">
-                <h4 className="project-title">AI Image Generator</h4>
-                <p className="description">
-                  Hobby projekt som skapar en bild med AI.
-                </p>
-              </div>
-              <div className="button-wrapper">
-                <button className="button-project">
-                  <a
-                    className="link"
-                    href="https://adamstubornzoo.netlify.app/"
-                    target="_blank"
-                  >
-                    Se hemsida
-                  </a>
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
-        <div
-          className="project-item"
-          onMouseOver={() => handleMouseOverZoo()}
-          onMouseLeave={() => handleMouseLeaveZoo()}
-        >
-          {showBacksideZoo ? (
-            <div className="frontside">
-              <img
-                src="/zoo.png"
-                alt="Josefins hemsida"
-                width={340}
-                height={240}
-              />
-            </div>
-          ) : (
-            <div className={`backside ${showBacksideZoo ? "visible" : ""}`}>
-              <div className="description-wrapper">
-                <h4 className="project-title">The Zoo</h4>
-                <p className="description">
-                  Inlämingsuppgift där vi skulle hämta djur från ett api och
-                  sedan hålla koll på när djuren ska matas i React
-                </p>
-              </div>
-              <div className="button-wrapper">
-                <button className="button-project">
-                  <a
-                    className="link"
-                    href="https://adamstubornzoo.netlify.app/"
-                    target="_blank"
-                  >
-                    Se hemsida
-                  </a>
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
+        <ProjectItem
+          title="Examensarbete"
+          image="/joes.png"
+          description="Som en del av mitt examensarbete har jag utvecklat en webbaserad konstnärlig portfolio för min syster."
+          link="https://josefinstuborn.netlify.app/"
+          isVisible={true}
+          buttonText="Se hemsida"
+        />
 
-        <div
-          className="project-item"
-          onMouseOver={() => handleMouseOverGodis()}
-          onMouseLeave={() => handleMouseLeaveGodis()}
-        >
-          {showBacksideGodis ? (
-            <div className="frontside">
-              <img
-                src="/godis.png"
-                alt="Josefins hemsida"
-                width={340}
-                height={240}
-              />
-            </div>
-          ) : (
-            <div className={`backside ${showBacksideGodis ? "visible" : ""}`}>
-              <div className="description-wrapper">
-                <h4 className="project-title">Godisbutiken</h4>
-                <p className="description">
-                  Grupparbete, att bygga en webshop i React
-                </p>
-              </div>
-              <div className="button-wrapper">
-                <button className="button-project">
-                  <a
-                    className="link"
-                    href="https://adamstuborngodisbutiken.netlify.app/"
-                    target="_blank"
-                  >
-                    Se hemsida
-                  </a>
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
-        <div
-          className="project-item"
-          onMouseOver={() => handleMouseOverTicTacToe()}
-          onMouseLeave={() => handleMouseLeaveTicTacToe()}
-        >
-          {showBacksideTicTacToe ? (
-            <div className="frontside">
-              <img
-                src="/tictactoe.png"
-                alt="Tic Tac Toe!"
-                width={340}
-                height={240}
-              />
-            </div>
-          ) : (
-            <div
-              className={`backside ${showBacksideTicTacToe ? "visible" : ""}`}
-            >
-              <div className="description-wrapper">
-                <h4 className="project-title">Tic Tac Toe!</h4>
-                <p className="description">
-                  Inlämningsuppgift i att skapa spelet Tic Tac Toe i Vue
-                </p>
-              </div>
-              <div className="button-wrapper">
-                <button className="button-project">
-                  <a
-                    className="link"
-                    href="https://github.com/Stuuben/TicTacToe"
-                    target="_blank"
-                  >
-                    Se Github
-                  </a>
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
-        <div
-          className="project-item"
-          onMouseOver={() => handleMouseOverResturant()}
-          onMouseLeave={() => handleMouseLeaveResturant()}
-        >
-          {showBacksideResturant ? (
-            <div className="frontside">
-              <img
-                src="/resturant.png"
-                alt="Resturant"
-                width={340}
-                height={240}
-              />
-            </div>
-          ) : (
-            <div
-              className={`backside ${showBacksideResturant ? "visible" : ""}`}
-            >
-              <div className="description-wrapper">
-                <h4 className="project-title">The Resturant</h4>
-                <p className="description">
-                  Grupparbete, Skapa ett bokningssystem för en resturang i React
-                </p>
-              </div>
-              <div className="button-wrapper">
-                <button className="button-project">
-                  <a
-                    className="link"
-                    href="https://github.com/annaboye/therestaurant-react"
-                    target="_blank"
-                  >
-                    Se Github
-                  </a>
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
+        <ProjectItem
+          title="AI Image Generator"
+          image="/generator.png"
+          description="Hobby projekt som skapar en bild med AI."
+          link="https://github.com/Stuuben/Ai-image-generator"
+          isVisible={true}
+          buttonText="Se Github"
+        />
+        <ProjectItem
+          title="Zoo"
+          image="/zoo.png"
+          description="Inlämingsuppgift, hämta djur från ett api och sedan hålla koll på när djuren ska matas i React"
+          link="https://adamstubornzoo.netlify.app/"
+          isVisible={true}
+          buttonText="Se hemsida"
+        />
+        <ProjectItem
+          title="TicTacToe"
+          image="/tictactoe.png"
+          description="Inlämningsuppgift, Skapa spelet Tic Tac Toe i Vue"
+          link="https://github.com/Stuuben/TicTacToe"
+          isVisible={true}
+          buttonText="Se Github"
+        />
+        <ProjectItem
+          title="Resturant"
+          image="/resturant.png"
+          description="Grupparbete, Skapa ett bokningssystem för en resturang i React"
+          link="https://github.com/annaboye/therestaurant-react"
+          isVisible={true}
+          buttonText="Se Github"
+        />
+        <ProjectItem
+          title="Godisbutiken"
+          image="/godis.png"
+          description="Grupparbete, att bygga en webshop i React"
+          link="https://adamstuborngodisbutiken.netlify.app/"
+          isVisible={true}
+          buttonText="Se hemsida"
+        />
       </div>
     </div>
   );
